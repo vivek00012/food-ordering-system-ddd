4ec8132a574ca217db840cb83a1124a02e0119bb
vivek00012 <35500124+vivek00012@users.noreply.github.com>
2024-09-29 21:09:16 +0530

    cqrs-implementatuib

---
+++ payment-service/payment-application/src/main/java/com/food/ordering/system/payment/service/application/rest/PaymentController.java
@@ -0,0 +1,7 @@
+package com.food.ordering.system.payment.service.application.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+}
---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/input/service/PaymentApplicationService.java
@@ -0,0 +1,4 @@
+package com.food.ordering.system.payment.service.domain.ports.input.service;
+
+public interface PaymentApplicationService {
+}
